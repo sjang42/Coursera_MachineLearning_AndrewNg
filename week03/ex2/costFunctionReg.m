91082e4e97a1c6fa258f3e97c897a4f449dbd946
sjang42 <jexful92@gmail.com>
2018-02-03 01:23:53 -0800

    [Complete week3] === - - - **What : finished all functhions** - cost/gradient functions - plot data - predict - sigmoid
    - - -
    *sjang42 <jexjang92@gmail.com>*

--- week03/ex2/costFunctionReg.m
+++ week03/ex2/costFunctionReg.m
@@ -10,6 +10,7 @@ m = length(y); % number of training examples
 % You need to return the following variables correctly 
 J = 0;
 grad = zeros(size(theta));
+size(grad);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -17,9 +18,16 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+J = 1 / m * sum(-y .* log(sigmoid(X * theta))\
+        - (1 - y) .* log(1 - sigmoid(X * theta)));
 
+theta0 = theta(1);
+J += lambda / (2 * m) * (sum(theta .^ 2) - theta0 ^ 2);
 
-
+grad = 1 / m * transpose(sigmoid(X * theta) - y) * X +\
+        transpose(((lambda / m) * theta));
+grad(1) -= lambda / m * theta0;
+grad = transpose(grad);
 
 
 % =============================================================
