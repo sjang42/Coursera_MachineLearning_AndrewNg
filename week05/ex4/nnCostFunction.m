402edf78215ccd7dc9ecbf7b0717079ba10d839c
sjang42 <jexful92@gmail.com>
2018-02-12 16:26:10 -0800

    [Complete week05] === - - - **What : Implement Neural Network Learning using Backpropagation** - nnCostFunction.m - randInitailizeWeights.m - sigmoidGradient.m
    - - -
    *sjang42 <jexjang92@gmail.com>*

--- week05/ex4/nnCostFunction.m
+++ week05/ex4/nnCostFunction.m
@@ -24,7 +24,7 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 
 % Setup some useful variables
 m = size(X, 1);
-         
+
 % You need to return the following variables correctly 
 J = 0;
 Theta1_grad = zeros(size(Theta1));
@@ -63,22 +63,45 @@ Theta2_grad = zeros(size(Theta2));
 %
 
 
+%% implement gradient
 
 
+n = size(X, 2) + 1;
+h = hidden_layer_size;
+r = num_labels;
 
+y_matrix = eye(num_labels)(y,:);
 
+a1 = [ones(m, 1) X];
 
+z2 = a1 * transpose(Theta1);
+a2 = sigmoid(z2);
+a2 = [ones(size(a2, 1), 1) a2];
 
+z3 = a2 * transpose(Theta2);
+a3 = sigmoid(z3);
 
+J = 1/m * sum(sum((-y_matrix .* log(a3)) - ((1 - y_matrix) .* log(1 - a3))));
 
+regularize = lambda / (2 * m) * ...
+  (sum(sum(Theta1(:,2:end).^2)) + sum(sum(Theta2(:,2:end).^2)));
 
+J += regularize;
 
+d3 = a3 - y_matrix;
+d2 = d3 * Theta2(:, 2:end) .* sigmoidGradient(z2);
 
+Delta1 = transpose(d2) * a1;
+Delta2 = transpose(d3) * a2;
 
+Theta1_grad = Delta1 / m;
+Theta2_grad = Delta2 / m;
 
+Theta1_grad += lambda / m * Theta1;
+Theta1_grad(:,1) -= lambda / m * Theta1(:,1);
 
-
-
+Theta2_grad += lambda / m * Theta2;
+Theta2_grad(:,1) -= lambda / m * Theta2(:,1);
 
 % -------------------------------------------------------------
 
